d438b6bab7080d3e3037ab7b7cd11b1a72160334
192210288 <129162561+192210288@users.noreply.github.com>
2024-02-08 09:58:44 +0530

    Add files via upload

---
+++ greatest three numbers .py
@@ -0,0 +1,12 @@
+num1=3
+num2=4
+num3=6
+if(num1>=num2) and (num1>=num2):
+    largest=num1
+elif(num2>=num1) and (num3>=num1):
+        largest=num2
+else:
+            largest=num3
+            print("the laregst number is ",largest)
+    
+
