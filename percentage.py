ab0fc9a0931f86bdd4f48d63dd29a1ccb3f48d2b
192210288 <129162561+192210288@users.noreply.github.com>
2024-02-08 10:02:58 +0530

    Add files via upload

---
+++ percentage.py
@@ -0,0 +1,13 @@
+percentage=int(input("enter a number"))
+if(percentage>=90):
+    print("outstanding")
+elif(percentage>=80):
+    print("excellent")
+elif(percentage>=70):
+    print("verygood")
+elif(percentage>=60):
+    print("good")
+elif(percentage>50):
+    print("pass")
+else:
+    print("fail")
