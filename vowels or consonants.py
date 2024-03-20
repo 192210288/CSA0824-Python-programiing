a8f870752c4f844e97e59d78e76a0a19eb834461
192210288 <129162561+192210288@users.noreply.github.com>
2024-03-20 10:11:03 +0530

    Add files via upload

---
+++ vowels or consonants.py
@@ -0,0 +1,8 @@
+def vowelorconsonants(x):
+ x=char(input("enter a string"))
+count=0
+if(x=='a' or x=='e' or x=='i' or x=='o' or x=='u'):
+        count+=1
+        print("vowels")
+else:
+        print("consonants")
