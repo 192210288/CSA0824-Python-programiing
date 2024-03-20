bdf3d03be287d02a8761a67004c51d3e2a7846cd
192210288 <129162561+192210288@users.noreply.github.com>
2024-03-20 10:12:30 +0530

    Add files via upload

---
+++ total in loop.py
@@ -0,0 +1,8 @@
+num=int(input("enter a number"))
+num=int(input("enter a number"))
+num=int(input("enter a number"))
+num=int(input("enter a number"))
+total=0
+while num !=0:
+    total+=num
+    print("the sum is",total)
