80305b2c980a670fd80906a424bde8dcafa1eabf
192210288 <129162561+192210288@users.noreply.github.com>
2024-03-20 10:14:29 +0530

    Add files via upload

---
+++ string is accepted or not.py
@@ -0,0 +1,10 @@
+import re
+def find(string):
+   if special_char==re.compile('[@!#$%^&*()_{}:<>?/|~]'):
+      if special_char.search(string)==none:
+        return"string is accepted"
+      else:
+        return "string is not accepted"
+s="hello15"
+print(s)
+print(find(s))
